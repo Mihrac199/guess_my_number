6e9569300e54fb4546efd334b1442eb5de5b6b92
Mihrac199 <mihrac.comart1996@gmail.com>
2025-03-19 16:43:33 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Guess My Number Project
+
+Find the given number by guessing.
---
+++ script.js
@@ -0,0 +1,78 @@
+'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+let guess = Number(document.querySelector(".guess").value);
+
+const displayMessage = function (message) {
+    document.querySelector(".message").textContent = message;
+}
+
+
+// Click the check
+document.querySelector(".check").addEventListener("click",
+    function () {
+
+        guess = Number(document.querySelector(".guess").value);
+
+        // Not is input
+        if (!guess) {
+            displayMessage("No Number");
+        }
+
+        // İf the number entered is not a secret number
+        else if (guess !== secretNumber) {
+            if (score > 1) {
+                displayMessage(guess > secretNumber ? "Too High!" : "Too Low!");
+
+                score--;
+
+                document.querySelector(".score").textContent = score;
+            }
+
+            else {
+                displayMessage("You lost the game");
+                document.querySelector(".score").textContent = 0;
+            }
+        }
+
+        // Player is win
+        else if (guess === secretNumber) {
+            displayMessage("Correct Number");
+
+            document.querySelector(".number").textContent = secretNumber;
+
+            document.querySelector("body").style.backgroundColor = "#60b347";
+
+            document.querySelector(".number").style.width = "30rem";
+
+            if (score > highscore) {
+                highscore = score;
+            }
+
+            document.querySelector(".highscore").textContent = highscore;
+        }
+    });
+
+
+// Click the again
+document.querySelector(".again").addEventListener("click",
+    function () {
+
+        secretNumber = Math.trunc(Math.random() * 20) + 1;
+        score = 20;
+
+        document.querySelector("body").style.backgroundColor = "#222";
+
+        guess = Number(document.querySelector(".guess").value = "");
+
+        document.querySelector(".number").style.width = "15rem";
+
+        displayMessage("Start guessing...");
+
+        document.querySelector(".score").textContent = score;
+
+        document.querySelector(".number").textContent = "?";
+    });
